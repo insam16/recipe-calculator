842ea1872ac80fafe9b3aaf987201b55c8141f54
yuna <yumin5541@naver.com>
2026-05-16 22:40:59 +0900

    style: 사진에 툴팁 추가

--- src/features/calculator/components/ItemButton.tsx
+++ src/features/calculator/components/ItemButton.tsx
@@ -24,6 +24,16 @@ export default function ItemButton({ item, isSelected, onClick }: Props) {
           height={60}
         />
       </button>
+
+      {/* Tooltip */}
+      <div className="
+        absolute bottom-full mb-2 left-1/2 -translate-x-1/2
+        px-2 py-1 text-xs text-white bg-black rounded
+        opacity-0 group-hover:opacity-100
+        transition pointer-events-none whitespace-nowrap
+      ">
+        {item.name}
+      </div>
     </div>
   )
 }
